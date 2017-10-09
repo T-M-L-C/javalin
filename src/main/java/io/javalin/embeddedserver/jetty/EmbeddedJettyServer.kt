28e8b615b967cc4e53c72ca561889b50ea26619d
David <davidaase@hotmail.com>
2017-10-10 00:53:35 +0200

    [embedded-jetty] Wrap service-call in try

--- src/main/java/io/javalin/embeddedserver/jetty/EmbeddedJettyServer.kt
+++ src/main/java/io/javalin/embeddedserver/jetty/EmbeddedJettyServer.kt
@@ -31,11 +31,15 @@ class EmbeddedJettyServer(private val server: Server, private val javalinServlet
         val httpHandler = object : SessionHandler() {
             override fun doHandle(target: String, jettyRequest: Request, request: HttpServletRequest, response: HttpServletResponse) {
                 if (request.isWebSocket()) return // don't touch websocket requests
-                javalinServlet.service(request.apply {
-                    setAttribute("jetty-target", target)
-                    setAttribute("jetty-request", jettyRequest)
-                }, response)
-                jettyRequest.isHandled = true
+                try {
+                    javalinServlet.service(request.apply {
+                        setAttribute("jetty-target", target)
+                        setAttribute("jetty-request", jettyRequest)
+                    }, response)
+                    jettyRequest.isHandled = true
+                } catch (e: Exception) {
+                    log.error("Exception occurred while servicing request", e)
+                }
             }
         }
 
